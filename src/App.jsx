289815225b7885523bd978a993601f936037d936
bobby <ramdesai9600@gmail.com>
2024-09-26 19:04:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import About from './component/About'
+import Contact from './component/Contact'
+import Footer from './component/Footer'
+import Hero from './component/Hero'
+import Navbar from './component/Navbar'
+import Projects from './component/Projects'
+import Service from './component/Service'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Service/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import HeroImg from "../assets/Hero.jpg"
+
+const Hero=()=>{
+    return(
+        <div className="bg-black text-white text-center py-16">
+             <img src={HeroImg}alt="" className="mx-auto mb-8 w-48 h-48 rounded-full object-cover transform
+             transition-transform duration-300 hover:scale-105"/>
+             < h1 className="text-4xl font-bold">
+                I am {""}
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">Ramchandra Desai</span>
+                ,Full-Stack Developer
+             </h1>
+             <p className="mt-4 text-lg text-gray-300 p">
+                I specialize in building modern and responsive web applications.
+             </p>
+             <div className="mt-8 space-x-4">
+                <button 
+                className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline 
+                 transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">Contact With Me</button>
+                <button 
+                className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline 
+                 transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">Resume</button>
+             </div>
+        </div>
+
+    )
+}
+
+export default Hero
